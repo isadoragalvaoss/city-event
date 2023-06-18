8eaf4e9add5541bf9576008cc8848d94c830252b
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-06-18 02:17:43 -0300

    feat: UI Kitten Provider and react-hook-form

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+import * as eva from "@eva-design/eva";
 import { NavigationContainer } from "@react-navigation/native";
+import { ApplicationProvider } from "@ui-kitten/components";
 import Stacks from "./src/screens/Stacks";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stacks />
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Stacks />
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
