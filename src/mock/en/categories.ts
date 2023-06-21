acea7e2bdca778f02445c47e08f16f8a7426a6c1
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-06-21 02:16:38 -0300

    feat: internationalization of mocks and new translations

---
+++ src/mock/en/categories.ts
@@ -0,0 +1,28 @@
+export const categoriesEN = [
+  {
+    id: 1,
+    title: "Lectures",
+  },
+  {
+    id: 2,
+    title: "Conferences",
+  },
+  {
+    id: 3,
+    title: "Courses",
+  },
+  {
+    id: 4,
+    title: "Trade Shows",
+  },
+
+  {
+    id: 5,
+    title: "Training",
+  },
+
+  {
+    id: 6,
+    title: "Bootcamps",
+  },
+];
---
+++ src/mock/pt/categories.ts
@@ -0,0 +1,28 @@
+export const categoriesPT = [
+  {
+    id: 1,
+    title: "Palestras",
+  },
+  {
+    id: 2,
+    title: "Congressos",
+  },
+  {
+    id: 3,
+    title: "Cursos",
+  },
+  {
+    id: 4,
+    title: "Feiras",
+  },
+
+  {
+    id: 5,
+    title: "Treinamentos",
+  },
+
+  {
+    id: 6,
+    title: "Bootcamps",
+  },
+];
